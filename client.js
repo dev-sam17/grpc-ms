dfdfc34709e030ca8363911ab4cbae28e3954954
Developer Sam <dev_sam17@protonmail.com>
2023-04-16 09:10:32 +0000

    Moved promise functions from test to client

--- client.js
+++ client.js
@@ -16,6 +16,64 @@ const product_proto = grpc.loadPackageDefinition(packageDefinition).product;
 
 const client = new product_proto.Products('localhost:3000', grpc.credentials.createInsecure());
 
-module.exports = client
+// get products
+function GetProducts() {
+  return new Promise((resolve, reject) => {
+      client.GetProducts({}, (error, response) => {
+          if (response) {
+              resolve(response)
+          } else {
+              reject(error)
+          }
+      })
+  })
+}
+
+// get product by id
+function GetProductById(id) {
+  return new Promise((resolve, reject) => {
+      client.GetProductById({id:id}, (error, response) => {
+          if (response) {
+              resolve(response)
+          } else {
+              reject(error)
+          }
+      })
+  })
+}
+
+// create product
+function CreateProduct(name, price) {
+  return new Promise((resolve, reject) => {
+      client.CreateProduct({name: name,
+      price: price}, (error, response) => {
+          if (response) {
+              resolve(response)
+          } else {
+              reject(error)
+          }
+      })
+  })
+}
+
+// delete product by id
+function DeleteProduct(id) {
+  return new Promise((resolve, reject) => {
+      client.DeleteProductById({id: id}, (error, response) => {
+          if (response) {
+              resolve(response)
+          } else {
+              reject(error)
+          }
+      })
+  })
+}
+
+module.exports = {
+  GetProducts,
+  GetProductById,
+  CreateProduct,
+  DeleteProduct
+}
 
                                     
--- test.js
+++ test.js
@@ -1,23 +1,11 @@
 const client = require('./client')
 
+
 // get products
-function newFunctionGetProducts() {
-    return new Promise((resolve, reject) => {
-        client.GetProducts({}, (error, response) => {
-            if (response) {
-                resolve(response)
-            } else {
-                reject(error)
-            }
-        })
-    })
-}
-
-
 async function getProductsFunction() {
     try {
         console.log('Getting Products')
-        const products = await newFunctionGetProducts()
+        const products = await client.GetProducts()
         console.log(products)
     } catch (error) {
         console.log("Error:" + error)
@@ -25,46 +13,22 @@ async function getProductsFunction() {
 }
 
 // get product by id
-function newFunctionGetProductById(id) {
-    return new Promise((resolve, reject) => {
-        client.GetProductById({id:id}, (error, response) => {
-            if (response) {
-                resolve(response)
-            } else {
-                reject(error)
-            }
-        })
-    })
-}
-
 async function getProductByIdFunction(id) {
     try {
         console.log('Getting Product By ID')
-        const product = await newFunctionGetProductById(id)
+        const product = await client.GetProductById(id)
         console.log(product)
     } catch (error) {
         console.log('Error : ' + error)
     }
 }
 
-// create product
-function newFunctionCreateProduct(name, price) {
-    return new Promise((resolve, reject) => {
-        client.CreateProduct({name: name,
-        price: price}, (error, response) => {
-            if (response) {
-                resolve(response)
-            } else {
-                reject(error)
-            }
-        })
-    })
-}
 
+// create product
 async function createProductFunction(name, price) {
     try {
         console.log('Creating Product')
-        const product = await newFunctionCreateProduct(name, price)
+        const product = await client.CreateProduct(name, price)
         console.log(product)
     } catch (error) {
         console.log('Error : ' + error)
@@ -72,22 +36,10 @@ async function createProductFunction(name, price) {
 }
 
 // delete product by id
-function newFunctionDeleteProduct(id) {
-    return new Promise((resolve, reject) => {
-        client.DeleteProductById({id: id}, (error, response) => {
-            if (response) {
-                resolve(response)
-            } else {
-                reject(error)
-            }
-        })
-    })
-}
-
 async function deleteProductFunction(id) {
     try {
         console.log('Deleting Product')
-        const product = await newFunctionDeleteProduct(id)
+        const product = await client.DeleteProduct(id)
         console.log(product)
     } catch (error) {
         console.log('Error : ' + error)
@@ -95,14 +47,7 @@ async function deleteProductFunction(id) {
 }
 
 
-
-
-
-
-
-
-
-// getProductsFunction()
-// getProductByIdFunction("6436e1a88e185939f6e100f5")
-// createProductFunction('Game of Thrones', '23.99')
-// deleteProductFunction('6436bcd2f7803f1509a94a50')
+getProductsFunction()
+// getProductByIdFunction("643186f89eb3ae1c9b9d265a")
+// createProductFunction('Lords of the Rings', '24.99')
+// deleteProductFunction('643186f89eb3ae1c9b9d265a')
